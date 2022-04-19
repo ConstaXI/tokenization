1aff56e30a0726d9f527bd76ec0749bc3c5aa9d6
Davi Banfi <davibanfi1579@outlook.com>
2022-04-19 14:05:52 -0300

    feat: add crowdsale smart contract

--- test/Token.test.js
+++ test/Token.test.js
@@ -12,11 +12,39 @@ chai.use(chaiAsPromised)
 const expect = chai.expect
 
 contract("Token Test", async (accounts) => {
+    const [deployerAccount, recipient, anotherAccount] = accounts
+    
     it("all tokens sould be in my account", async () => {
-        let instance = await Token.deployed()
+        const instance = await Token.deployed()
 
-        let totalSupply = await instance.totalSupply()
+        const totalSupply = await instance.totalSupply()
 
-        expect(await instance.balanceOf(accounts[0])).to.eventually.be.bignumber.equal(totalSupply)
+        expect(instance.balanceOf(deployerAccount)).to.eventually.be.a.bignumber.equal(totalSupply)
+    })
+
+    it("is not possible to send more tokens than available in total", async () => {
+        const instance = await Token.deployed()
+
+        const balanceOfDeployer = await instance.balanceOf(deployerAccount)
+
+        expect(instance.transfer(recipient, new BN(balanceOfDeployer + 1))).to.eventually.be.rejected
+
+        expect(instance.balanceOf(deployerAccount)).to.eventually.be.a.bignumber.equal(balanceOfDeployer)
+    })
+
+    it("is possible to send tokens between accounts", async () => {
+        const sendTokens = 1;
+
+        const instance = await Token.deployed()
+
+        const totalSupply = await instance.totalSupply()
+
+        expect(instance.balanceOf(deployerAccount)).to.eventually.be.a.bignumber.equal(totalSupply)
+
+        expect(instance.transfer(recipient, sendTokens)).to.eventually.be.fulfilled
+
+        expect(instance.balanceOf(deployerAccount)).to.eventually.be.a.bignumber.equal(totalSupply.sub(new BN(sendTokens)))
+
+        expect(instance.balanceOf(recipient)).to.eventually.be.a.bignumber.equal(new BN(sendTokens))
     })
 })
